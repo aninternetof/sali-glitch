95f1971e7a1e8e8555abf40f028fd227155df8b9
Glitch (dawn-crystal) <none>
2018-11-02 15:10:19 +0000

    ✂️🏜 Checkpoint ./src/containers/App.js:1529861/885

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,18 +3,19 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import actions from '../state/actions';
 import { Link } from 'react-router-dom'
+import { push } from 'connected-react-router'
 
 class App extends Component {
   
-  handleClick = () => {
-    this.props.history.push('/newthing');  
+  handleClick(event) {
+     this.props.navigate('/home');
   }
   
   render() {
     return (
       <div>
         <button onClick={this.props.setTest} >console.log(actions);</button>
-        <button onClick={this.props.setTest} >console.log(actions);</button>
+        <button onClick={this.handleClick} >Go</button>
         <Link to="/about">About</Link>
       </div>
     );
@@ -30,7 +31,8 @@ const mapStateToProps = (state={}) => {
 };
 
 const mapDispatchToProps = (dispatch) => ({
-  setTest: bindActionCreators(actions.setTest, dispatch)
+  setTest: bindActionCreators(actions.setTest, dispatch),
+  navigate: bindActionCreators(push, dispatch)
 });
 
 export default connect(
