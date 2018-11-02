cbadc1c786c7ca766912ce8b3397daa57ded1b9b
Glitch (dawn-crystal) <none>
2018-11-02 14:49:18 +0000

    🏅🌮 Checkpoint ./package.json:1529861/61 ./src/index.js:1529861/897

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import {createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux'
+import { Route, Switch } from 'react-router-dom';
+import { ConnectedRouter } from 'connected-react-router'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createLogger } from 'redux-logger';
 import promiseMiddleware from 'redux-promise-middleware';
 import thunk from 'redux-thunk';
+import { createBrowserHistory } from 'history'
+import { connectRouter, routerMiddleware } from 'connected-react-router'
 
 import './index.css';
 import App from './containers/App';
@@ -22,13 +26,22 @@ const reduxMiddleware = composeEnhancers(
 
 const preloadedState = {
 };
+const history = createBrowserHistory()
 
-const store = createStore(settings, preloadedState, reduxMiddleware);
+const store = createStore(
+  connectRouter(history)(settings),
+  preloadedState,
+  reduxMiddleware
+);
 
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route path="/:name?" component={App} />
+      </Switch>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
 );
