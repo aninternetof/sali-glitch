54bfe60dc1b61a99692ced94b3583ea02aa5c835
Glitch (dawn-crystal) <none>
2018-11-02 14:38:06 +0000

    🏈🐃 Checkpoint ./src/index.js:1529861/637 ./package.json:1529861/332

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import {createStore, compose } from 'redux';
+import {createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createLogger } from 'redux-logger';
+import promiseMiddleware from 'redux-promise-middleware';
+import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './containers/App';
 import { settings } from './state/reducers';
 
-//const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+/* eslint-disable-next-line no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const reduxMiddleware = composeEnhancers(
+  applyMiddleware(
+    thunk,
+    createLogger(),
+    promiseMiddleware(),
+  ),
+);
+
+const preloadedState = {
+};
 
 const store = createStore(settings, preloadedState, reduxMiddleware);
-persistStore(store)
 
 
 ReactDOM.render(
